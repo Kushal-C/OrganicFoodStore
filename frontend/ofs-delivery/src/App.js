364378f38f28a66eeba9ebf54bac1c52d670734d
Kushal Cuttari <kcutari@gmail.com>
2018-09-27 14:54:24 -0700

    setup route

--- frontend/ofs-delivery/src/App.js
+++ frontend/ofs-delivery/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './img/logo.svg';
+import { Route } from "react-router-dom";
+
+import SignIn from './components/sign_in';
 import './css/App.css'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route path="/" component={SignIn} />
       </div>
     );
   }
---
+++ frontend/ofs-delivery/src/components/sign_in.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class SignIn extends Component {
+    render(){
+        return(
+            <div>
+                <p>Hello World!</p>
+            </div>
+        );
+    }
+}
+export default SignIn;
