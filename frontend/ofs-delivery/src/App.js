dabde0e641c9c4b6787cc679d254e96f0f93fc2e
Farokh C <32556828+FarokhC@users.noreply.github.com>
2018-09-29 00:25:08 -0700

    Switch from BrowserRouter to HashRouter, remove useless import, change from class to function component

--- frontend/ofs-delivery/src/App.js
+++ frontend/ofs-delivery/src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import { Route } from "react-router-dom";
-
-import SignIn from './components/sign_in';
+import { HashRouter as Router, Route } from "react-router-dom";
 
 import './css/bootstrap.min.css'
 import './css/shards.min.css'
 import './css/App.css'
 
+import SignIn from './components/sign_in';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Route path="/" component={SignIn} />
-      </div>
-    );
-  }
-}
-
-export default App;
+const App = ()=> (
+  <Router>
+    <div className="App">
+      <Route path="/" component={SignIn} />
+    </div>
+  </Router>
+);
--- frontend/ofs-delivery/src/index.js
+++ frontend/ofs-delivery/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import ReactDOM, { render }  from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { render }  from 'react-dom';
 import './css/index.css';
 import App from './App';
 
-ReactDOM.render(
-                <BrowserRouter>
-                  <App/>
-                </BrowserRouter>, 
-        document.getElementById('root'));
+render(<App/>,document.getElementById('root'));
