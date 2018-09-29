ce75270a829d9ceb47e24ba0b785ac235a70d74a
Farokh C <32556828+FarokhC@users.noreply.github.com>
2018-09-29 15:36:53 -0700

    Added forms for the username and password fields, and a submit button

--- frontend/ofs-delivery/src/components/sign_in.js
+++ frontend/ofs-delivery/src/components/sign_in.js
@@ -4,7 +4,20 @@ class SignIn extends Component {
     render(){
         return(
             <div>
-                <p>Hello World!</p>
+                <h1>Organic Food Store Delivery Service</h1>
+                <form>
+                <label>
+                    User Name:
+                    <input type="text" name="name" />
+                </label>
+                <br></br>
+                <label>
+                    Password:
+                    <input type="text" name="name" />
+                </label>
+                <br></br>
+                <input type="submit" value="Submit" />
+                </form>
             </div>
         );
     }
