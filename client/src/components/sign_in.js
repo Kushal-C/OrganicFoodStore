b770505eddf6a10c1e0a8850241a1bff6d6244e3
Farokh C <32556828+FarokhC@users.noreply.github.com>
2018-10-01 21:15:00 -0700

    Added feature to post user email and password to /api/login

--- client/src/components/sign_in.js
+++ client/src/components/sign_in.js
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
+import axios from "axios";
 import { getItems } from "../actions/index";
 
 class SignIn extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email : "",
+      password : ""
+    }
+  }
+
   componentWillMount() {
     getItems();
+  }
+
+  handleEmailChange = event => this.state.email = event.target.value;
+
+  handlePasswordChange = event => this.state.password = event.target.value;
+
+  handleClick = event => {
+    axios.post('/api/login', this.state)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 
   render() {
@@ -15,12 +38,12 @@ class SignIn extends Component {
             <h4 className="card-title header-primary">LOGIN</h4>
             <div className="card-body">
               <label>User Name:</label>
-              <input type="text" name="name" />
+              <input type="text" name="email" onChange={this.handleEmailChange}/>
               <br />
               <label>Password:</label>
-              <input type="text" name="name" />
+              <input type="text" name="password" onChange={this.handlePasswordChange}/>
               <br />
-              <button className="btn btn-primary">LOGIN</button>
+              <button className="btn btn-primary" onClick={this.handleClick}>LOGIN</button>
             </div>
           </div>
         </div>
