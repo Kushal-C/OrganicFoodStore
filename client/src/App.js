da569ee3c8b03c4792358bd8a9c644502bad7541
Kushal Cuttari <kcutari@gmail.com>
2018-10-03 14:52:14 -0700

    Setup Redux and Redux Store and created sign in container

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import logger from "redux-logger";
+import promiseMiddleware from "redux-promise";
+import reducers from "./reducers/index";
+import SignInContainer from "./containers/sign_in_container";
 
-import "./css/bootstrap.min.css";
-import "./css/shards.min.css";
-import "./css/App.css";
-
-import SignIn from "./components/sign_in";
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, logger)(
+  createStore
+);
 
 class App extends Component {
   render() {
     return (
-    <div>
+      <Provider store={createStoreWithMiddleware(reducers)}>
         <Router>
           <div className="container-fluid">
-            <Route path="/" component={SignIn} />
+            <Route path="/" component={SignInContainer} />
           </div>
         </Router>
-    </div>);
+      </Provider>
+    );
   }
 }
 
