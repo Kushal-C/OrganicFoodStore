f7f02d5c4314572faa330544c4ef166627001fc0
Enrique <enriquecardenas96@gmail.com>
2018-10-03 23:46:01 -0700

    updated server.js

--- server.js
+++ server.js
@@ -3,14 +3,49 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+    res.send({ express: 'testing' });
+});
+
 app.get('/api/hello', (req, res) => {
     res.send({ express: 'Hello From Express' });
 });
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host     : '',
+    user     : 'masterUsername',
+    password : '',
+    database : ''
+  });
+ 
+  connection.connect(function(err){
+    if(!err) {
+        console.log("Database is connected ... nn");    
+    } else {
+        console.log(err);    
+    }
+    });
+
+  connection.query('SELECT * from Users', function(err, rows, fields) {
+  connection.end();
+    if (!err)
+      console.log('The solution is: ', rows);
+    else
+      console.log('Error while performing Query.');
+  });
+  
+
+/*
 
 app.post('/api/login', (req, res) => {
     console.log("Req");
     console.log(req.param('email'));
     res.json({responseCode: '200'});
 });
+
+*/
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
