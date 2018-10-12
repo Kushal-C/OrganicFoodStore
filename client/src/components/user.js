5545b95fffdf72d6ac8a3469ed83661f10ce43c8
Farokh C <32556828+FarokhC@users.noreply.github.com>
2018-10-12 00:46:32 -0700

    Removing dropdown html

--- client/src/components/user.js
+++ client/src/components/user.js
@@ -8,11 +8,7 @@ export default class User extends Component{
   render() {
     return(
       <div class="dropdown">
-      <span class="badge badge-primary">{this.props.name}</span>
-        <ul class="dropdown-menu">
-          <li><a href="#">Profile</a></li>
-          <li><a href="#">Settings</a></li>
-        </ul>
+        <span class="badge badge-primary">{this.props.name}</span>
       </div>
     )
   }
