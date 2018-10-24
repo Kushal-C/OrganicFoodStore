af354aefec192a79e7581d6067706a18b757ac5e
Kushal Cuttari <kcutari@gmail.com>
2018-10-24 14:10:37 -0700

    Server calls fixed

--- client/src/reducers/login_reducer.js
+++ client/src/reducers/login_reducer.js
@@ -1,8 +1,9 @@
 import { LOGIN_REQUEST } from "../actions/action_constants";
 
-export default function(state = null, action) {
+export default function(state = {}, action) {
   switch (action.type) {
     case LOGIN_REQUEST:
+      console.log("Got here");
       return action.payload.data;
     default:
       return state;
