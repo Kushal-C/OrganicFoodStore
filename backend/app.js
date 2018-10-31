89714fb1a29db6ae66b2613ba8782c16ebc1b8cb
Enrique <enriquecardenas96@gmail.com>
2018-10-31 00:42:47 -0700

    edited register route

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ var cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
+var registerRouter = require('./routes/register');
 
 var app = express();
 const port = process.env.PORT || 5000;
@@ -53,6 +54,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/login', loginRouter);
+app.use('/api/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
