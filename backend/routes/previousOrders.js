80956aa1023ce68391bb8a59d198956cc1af6359
Andre Chik <Andre@Andres-MacBook-Pro.local>
2018-11-04 13:55:12 -0800

    previousOrders route

---
+++ backend/routes/previousOrders.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var cors = require('cors')
+
+const database = require('./server_constants').mysql_pool;
+router.all('*', cors());
+
+router.post("/", (req, res, next) => {
+    console.log("Login button pressed");
+    
+      database.getConnection(function(err, connection){
+          var sql = "SELECT * FROM cart as c, (SELECT cartId FROM transaction WHERE userId = 1) as d WHERE c.userId = d.cartId";
+          connection.query(sql, function(err, result, fields){
+              if (err) throw err;
+              else {
+                console.log(JSON.stringify(fields));
+                
+                console.log("results: " + JSON.stringify(result));
+                res.send(JSON.stringify(result));
+              }
+          });
+      });
+  
+  });
+
+module.exports = router;  
