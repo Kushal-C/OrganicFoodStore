eee428b4212c06a852da86f8e7b2c08c7598f329
Kushal Cuttari <kcutari@gmail.com>
2018-11-10 13:50:11 -0800

    Fixed dashboard

--- client/src/containers/dashboard_container.js
+++ client/src/containers/dashboard_container.js
@@ -13,7 +13,10 @@ class DashboardContainer extends Component {
   }
   render() {
     //TODO: Replace item_props with a list of properties for each item to be rendered
-    return <Dashboard firstName={this.props.login[0].firstName} lastName={this.props.login[0].lastName} getItems={getItemsRequest} getProfile={getUserProfile} item_props={[{ name: "Broccoli", description: "Flower of Broccoli", image_link: "https://i5.walmartimages.ca/images/Large/950/304/6000016950304.jpg", cost: 2, weight: 1, weight_unit: "pound" }, { name: "Mango", description: "A single mango", image_link: "https://res.cloudinary.com/norgesgruppen/image/upload/c_fill,f_auto,h_439,q_auto,w_780/rqkghle0gmhvbiteusut.png", cost: 3, weight: 1, weight_unit: "pound" }]} />;
+    if(this.props.login[0] != null){
+          return <Dashboard firstName={this.props.login[0].firstName} lastName={this.props.login[0].lastName} getItems={getItemsRequest} getProfile={getUserProfile} item_props={[{ name: "Broccoli", description: "Flower of Broccoli", image_link: "https://i5.walmartimages.ca/images/Large/950/304/6000016950304.jpg", cost: 2, weight: 1, weight_unit: "pound" }, { name: "Mango", description: "A single mango", image_link: "https://res.cloudinary.com/norgesgruppen/image/upload/c_fill,f_auto,h_439,q_auto,w_780/rqkghle0gmhvbiteusut.png", cost: 3, weight: 1, weight_unit: "pound" }]} />;
+    }
+    return <Dashboard firstName={"John"} lastName={"Smith"} getItems={this.props.getItemsRequest} getProfile={this.props.getUserProfile} item_props={[{ name: "Broccoli", description: "Flower of Broccoli", image_link: "https://i5.walmartimages.ca/images/Large/950/304/6000016950304.jpg", cost: 2, weight: 1, weight_unit: "pound" }, { name: "Mango", description: "A single mango", image_link: "https://res.cloudinary.com/norgesgruppen/image/upload/c_fill,f_auto,h_439,q_auto,w_780/rqkghle0gmhvbiteusut.png", cost: 3, weight: 1, weight_unit: "pound" }]} />;
   }
 }
 
