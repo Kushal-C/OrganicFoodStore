7586232c8cf1ebec1909617cbc24f86cefd6cea3
Andre Chik <AndreC510@users.noreply.github.com>
2018-11-14 18:28:31 -0800

    Delete googleconfig.js

--- backend/config/googleconfig.js
+++
@@ -1,8 +0,0 @@
-const googleMapsClient = require('@google/maps').createClient({
-    key: 'AIzaSyAsbJmghb9NrphRYCTsdqP7NxwwlTVSiIM',
-    Promise: Promise
-  });
-
-const storeGeoLocs = ["402 N El Camino Real, San Mateo, CA 94401", "1984 Los Padres Blvd Santa Clara, CA 95050"];
-
-module.exports = {googleMapsClient, storeGeoLocs};
