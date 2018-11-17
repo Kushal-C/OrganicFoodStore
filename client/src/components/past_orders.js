3c2267222ede0446a6e90462485fe699c70159ae
Farokh C <32556828+FarokhC@users.noreply.github.com>
2018-11-16 20:54:15 -0800

    Connected past orders page to backend. pastorders route on server side must be properly implemented. (#108)

--- client/src/components/past_orders.js
+++ client/src/components/past_orders.js
@@ -11,15 +11,16 @@ export default class PastOrders extends Component {
       <div className="col-md-8">
     {this.props.orders.map(function(order){
       return (
-      
+
       <div className="col-md-12" style={{marginTop:'12px'}}>
         <Order
+          orderId = {order.orderId}
           status = {order.status}
           contents = {order.contents}
           total_cost = {order.total_cost}
         ></Order>
       </div>
-      
+
     )})}
     </div>
 </div>
