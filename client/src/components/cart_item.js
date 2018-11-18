07eb16f15773eddcf3e0d5fdb581f5dfcefdd1e7
Farokh C <32556828+FarokhC@users.noreply.github.com>
2018-11-18 15:13:13 -0800

    stopped cart page from crashing with emtpy cart

--- client/src/components/cart_item.js
+++ client/src/components/cart_item.js
@@ -1,37 +1,44 @@
 import React, { Component } from 'react';
 
 export default class CartItem extends Component{
+
+  displayCart() {
+    if(this.props.items) {
+    return (
+    this.props.items.map(function(item,key){
+      return(
+        <div key={key}>
+          <div className="row mr-2 ml-2" >
+
+            <div className="col-md-6">
+              {item.name} x {item.number}
+            </div>
+
+            <div className="col-md-6 text-right">
+              ${item.cost}
+            </div>
+
+            </div>
+              <div className="col-md-5 text-right">
+
+              </div>
+
+
+          <div className="text-center mb-3">
+            weight = {item.weight} {item.weight_unit}
+          </div>
+
+        </div>
+      )
+    })
+    )}
+  }
   render() {
     return(
       <div>
       <h4 className="head-title">Order Details</h4>
 
-        {this.props.items.map(function(item,key){
-            return(
-              <div key={key}>
-                <div className="row mr-2 ml-2" >
 
-                  <div className="col-md-6">
-                    {item.name} x {item.number}
-                  </div>
-
-                  <div className="col-md-6 text-right">
-                    ${item.cost}
-                  </div>
-
-                  </div>
-                    <div className="col-md-5 text-right">
-
-                    </div>
-
-
-                <div className="text-center mb-3">
-                  weight = {item.weight} {item.weight_unit}
-                </div>
-
-              </div>
-            )
-          })}
 
        </div>
     )
