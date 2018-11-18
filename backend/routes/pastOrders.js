37b1e8967706cc778489f91f72b0df0fccd01acc
Andre Chik <Andre@Andres-MacBook-Pro.local>
2018-11-18 12:13:04 -0800

    resolved pastOrders

--- backend/routes/pastOrders.js
+++ backend/routes/pastOrders.js
@@ -6,7 +6,6 @@ const database = require('../config/dbconfig').mysql_pool;
 router.all('*', cors());
 
 //need name of item, quantity, total cost of same product, total cost of all products
-<<<<<<< HEAD
 router.get("/", (req, res, next) => {
     var uId = req.body.userId;
     
@@ -23,60 +22,6 @@ router.get("/", (req, res, next) => {
             if (result.length > 0){
               // console.log("result: " +JSON.stringify(result));
               res.send(result);
-=======
-router.post("/", (req, res, next) => {
-    console.log("req.body: " + JSON.stringify(req.body));
-      database.getConnection(function(err, connection){
-        console.log("userId: " + req.body.userId)
-        // var cartQuery = "SELECT * FROM cart as c, (SELECT cartId FROM transaction WHERE userId = " + req.body.userId + ") as d WHERE c.cartId = d.cartId GROUP BY c.productId";
-        var cartQuery = "SELECT cartId, transactionId, SUM(total_weight) as 'total_weight', SUM(total_cost) as 'total_cost' \
-                        FROM cart as c, (SELECT cartId as cId, transactionId as tId FROM transaction WHERE userId = " + req.body.userId + ") as d WHERE c.cartId = d.cId AND c.transactionId = d.tId GROUP BY c.cartId"
-          connection.query(cartQuery, function(err, result){
-              if (err) throw err;
-              if (result.length > 0){
-                // console.log("result: " + JSON.stringify(result));
-                result = {data:
-                  [
-                    {
-                      orderId : 1,
-                      status : "processed",
-                      contents:
-                      [
-                        {
-                          name: "cake",
-                          quantity : 5,
-                          cost: 20
-                        },
-                        {
-                          name: "pie",
-                          quantity : 5,
-                          cost: 20
-                        }
-                      ],
-                      total_cost : 40
-                    },
-                    {
-                      orderId : 2,
-                      status : "processed",
-                      contents:
-                      [
-                        {
-                          name: "Brocolli",
-                          quantity : 10,
-                          cost: 20
-                        },
-                        {
-                          name: "Mango",
-                          quantity : 2,
-                          cost: 4
-                        }
-                      ],
-                      total_cost : 40
-                    }
-                  ]
-                }
-                res.send(result);
->>>>>>> 1386e780f1f6f3471db4a38d667e95f75e528633
             }
             else{
               res.send({responseCode: "404", reason:"No past orders made by this user"});
@@ -85,8 +30,4 @@ router.post("/", (req, res, next) => {
       });
   });
 
-<<<<<<< HEAD
 module.exports = router; 
-=======
-module.exports = router;
->>>>>>> 1386e780f1f6f3471db4a38d667e95f75e528633
