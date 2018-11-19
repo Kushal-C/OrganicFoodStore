b09e514d79134d73d57b0a70b31c43259759c4cc
Kushal Cuttari <kcutari@gmail.com>
2018-11-18 20:50:06 -0800

    Changed some itemcard stuff

--- client/src/components/dashboard/item_card.js
+++ client/src/components/dashboard/item_card.js
@@ -4,33 +4,30 @@ export default class ItemCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: this.props.name,
+      name: this.props.productName,
       quantity : 1
-    }
-    
+    };
     this.onChange = this.onChange.bind(this);
     this.sendAddToCartReq = this.sendAddToCartReq.bind(this);
 
   }
 
   onChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState({quantity: event.target.value});
+
   }
 
   sendAddToCartReq() {
-    this.props.addToCart({
-      name : this.props.name,
-      quantity: parseInt(this.state.quantity)
-    });
+    this.props.addToCart(this.state);
   }
 
   render() {
     return (
-      <div className="col-md-3" >
+      <div className="col-md-3"  >
         <div className="card " style={{width:'220px', height:'400px'}}>
           <img className="card-img-top" src={this.props.imageLink} alt="Category" style={{ height: '400px',width: '220px', overflow: 'hidden', objectFit:'cover'}}></img>
           <div className="card-body">
-            <h4 className="card-title">{this.props.name}</h4>
+            <h4 className="card-title">{this.props.productName}</h4>
             <p className="card-text">{this.props.description}</p>
             <div className="row" style={{borderTop: '1px solid rgb(231, 231, 231)', marginBottom: '10px'}}>
              <div className="col-sm-12 col-md-6 text-left">${this.props.cost}</div>
@@ -49,8 +46,8 @@ export default class ItemCard extends Component {
                   </button>
                 </div>
                 <div className="modal-body">
-                  <p>How many items of {this.props.name} do you want to order?</p>
-                  <input type="text" name="quantity" value = {this.state.quantity} onChange={this.onChange}></input>
+                    <p>How many items of {this.props.productName} do you want to order?</p>
+                  <input type="text" name="quantity" value ={this.state.quantity} onChange={this.onChange}></input>
                 </div>
                 <div className="modal-footer">
                   <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={this.sendAddToCartReq}>Add to Cart</button>
