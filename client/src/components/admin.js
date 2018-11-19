85795e47cb98414d9fe17d5eb270487f5e5065f9
Farokh C <32556828+FarokhC@users.noreply.github.com>
2018-11-18 18:59:45 -0800

    added more features

--- client/src/components/admin.js
+++ client/src/components/admin.js
@@ -3,12 +3,14 @@ import AdminItem from "./admin_item";
 
 export default class Admin extends Component {
 
-
-
-  render() {
-    return (
-      <AdminItem />
-    );
+  renderItems() {
+    this.props.items.map(function(item){
+      return (
+        <AdminItem name = {item.name} quantity = {item.quantity} updateQuantity = {this.props.updateQuantity} />
+    )})
   }
 
+  render() {
+    return this.renderItems();
+  }
 }
--- client/src/components/admin_item.js
+++ client/src/components/admin_item.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 
 export default class AdminItem extends Component {
-
   render() {
     return (
       <div className="card">
         <div className="text-left">
           <div>{this.props.name}</div>
+          <div>Left</div>
         </div>
         <div className="text-right">
           <div>{this.props.quantity}</div>
+          <div>Right</div>
+          <div className="col text-right">
+          <button
+            className="btn btn-primary"
+            onClick={() =>this.props.updateQuantity()}
+          >
+            Update Quantity
+          </button>
+        </div>
         </div>
       </div>
     );
