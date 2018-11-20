1a26245c105741f80db683f460b47e4749ab7cfa
Enrique <enriquecardenas96@gmail.com>
2018-11-20 15:50:01 -0800

    made register detect if email exists

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -9,31 +9,48 @@ const database = require('../config/dbconfig').mysql_pool;
 router.post("/", (req, res) => {
     database.getConnection((err, connection) => {
         if (err) throw err;
-        console.log("Connected!");
-        const sql = "INSERT INTO user (firstName, lastName, email, phoneNumber, password, address, city, state, zipcode, creditCardNumber, expirationDate, cvc) \
-                        VALUES('" +
-                            req.body.firstName + "', '" + // 'firstName', '
-                            req.body.lastName + "', '" + // lastName', '
-                            req.body.email + "', '" +
-                            req.body.phoneNumber  + "', '" +
-                            req.body.password + "', '" +
-                            req.body.address + "', '" +
-                            req.body.city  + "', '" +
-                            req.body.state  + "', '" +
-                            req.body.zipcode  + "', '" +
-                            req.body.cardNumber + "', '" +
-                            req.body.expiryDate  + "', '" +
-                            req.body.cvc + "')";
 
-        connection.query(sql, (error, result) => {
-            // When done with the connection, release it.
-            connection.release();
-            // Handle error after the release.
-            if (error) throw error;
+        // Check if email exists
+        connection.query("SELECT * FROM `user` WHERE email='" + req.body.email + "';", (error, result, fields) => {
 
-            console.log("1 record inserted");
+            if(result){ // Email exists in the database
+                
+                // Release connection
+                connection.release();
+                // Handle error after the release.
+                if (error) throw error;
+
+                // Send 409 Conflict JSON
+                res.send({ responseCode: "409" }); 
+            }
+
+            else{ // Email does not exist, create a new account
+                const sql = "INSERT INTO user (firstName, lastName, email, phoneNumber, password, address, city, state, zipcode, creditCardNumber, expirationDate, cvc) \
+                                VALUES('" +
+                                    req.body.firstName + "', '" + // 'firstName', '
+                                    req.body.lastName + "', '" + // lastName', '
+                                    req.body.email + "', '" +
+                                    req.body.phoneNumber  + "', '" +
+                                    req.body.password + "', '" +
+                                    req.body.address + "', '" +
+                                    req.body.city  + "', '" +
+                                    req.body.state  + "', '" +
+                                    req.body.zipcode  + "', '" +
+                                    req.body.cardNumber + "', '" +
+                                    req.body.expiryDate  + "', '" +
+                                    req.body.cvc + "')";
+
+                connection.query(sql, (error, result) => {
+                    // When done with the connection, release it.
+                    connection.release();
+                    // Handle error after the release.
+                    if (error) throw error;
+                    console.log("1 record inserted");
+                });
+
+            }
         });
-    });
+     });
 });
 
 
