52853b1d959f6db934c68bd2b067ebaf54a34390
Farokh C <32556828+FarokhC@users.noreply.github.com>
2018-11-20 15:06:40 -0800

    Prevents admin from making negative item quantity

--- client/src/components/admin_item.js
+++ client/src/components/admin_item.js
@@ -5,14 +5,14 @@ export default class AdminItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      newQuantity : "",
-      currentQuantity : this.props.quantity
+      newQuantity : 0,
+      currentQuantity : parseInt(this.props.quantity)
     }
 
     this.onChange = this.onChange.bind(this);
   }
 
-  changeQuantity() {
+  update() {
     let req = {
       productId : this.props.productId,
       quantity: this.state.newQuantity
@@ -21,6 +21,17 @@ export default class AdminItem extends Component {
     adminAddReq(req);
     let newVal = Number(this.state.currentQuantity) + Number(this.state.newQuantity);
     this.setState({currentQuantity : newVal});
+  }
+
+  changeQuantity() {
+    if(Number(parseInt(this.state.newQuantity) < 0)) {
+      if((Number(parseInt(this.state.currentQuantity)) + Number(parseInt(this.state.newQuantity))) >= 0) {
+        this.update();
+      }
+    }
+    else {
+      this.update();
+    }
   }
 
   onChange(event) {
