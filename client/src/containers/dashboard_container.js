30c6db4da311d3d9727f5d4f0aef18d0e6c7330f
Farokh C <32556828+FarokhC@users.noreply.github.com>
2018-12-01 14:25:57 -0800

    Fixed login page to check user info for authentication

--- client/src/containers/dashboard_container.js
+++ client/src/containers/dashboard_container.js
@@ -33,25 +33,6 @@ class DashboardContainer extends Component {
         <div>Loading</div>
       );
     }
-      // <Dashboard
-      //   firstName={"John"}
-      //   lastName={"Smith"}
-      //   getItems={this.props.getItems}
-      //   getProfile={this.props.getUserProfile}
-      //   match={this.props.match}
-      //   item_props={[
-      //     {
-      //       name: "Loading",
-      //       description: "Loading",
-      //       imageLink:
-      //         "https://i5.walmartimages.ca/images/Large/950/304/6000016950304.jpg",
-      //       cost: 2,
-      //       weight: 1,
-      //       weight_unit: "pound"
-      //     }]}
-      // />
-    // );
-
   }
 }
 
